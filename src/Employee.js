baa0a0414d13efb5d0fe9930d1a83215651f4ff9
swathysroy <swathysroy@gmail.com>
2021-02-05 15:35:33 +0530

    1 file modified

--- src/Employee.js
+++ src/Employee.js
@@ -4,23 +4,72 @@ import PropTypes from "prop-types";
 class Employee extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Employee component</h2>
-        <p>To be completed</p>
+      <div className="employee">
+        <h1>Employee</h1>
+        <label for="empid">
+          <b>Employee ID: </b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Employee Id"
+          name="empid"
+          value={this.props.empid}
+        />
+        <br />
+        <br />
+        <label for="name">
+          <b>Name: </b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Name"
+          name="name"
+          value={this.props.name}
+        />
+        <br />
+        <br />
+        <label for="deptname">
+          <b>Department Name: </b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Department Name"
+          name="deptname"
+          value={this.props.deptname}
+        />
+        <br />
+        <br />
+        <label for="gender">
+          <b>Gender: </b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Gender"
+          name="name"
+          value={this.props.gender}
+        />
+        <br />
+        <br />
+        <label for="age">
+          <b>Age: </b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Age"
+          name="age"
+          value={this.props.age}
+        />
       </div>
     );
   }
 }
 
 Employee.propTypes = {
-  empid : PropTypes.number.isRequired,
-  name : PropTypes.string.isRequired,
-  deptname: PropTypes.oneOf([
-    'HR','SALES','PROD'
-  ]), 
-  gender: PropTypes.oneOf([
-    'female', 'male'
-  ])
+  empid: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  deptname: PropTypes.oneOf(["HR", "SALES", "PROD"]),
+  gender: PropTypes.oneOf(["Female", "Male"]),
+  age: PropTypes.number
 };
 
 Employee.defaultProps = {
